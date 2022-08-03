b3e8933f5d27543e1f1ac6023a4260c999947213
jjz62 <jjz62@cornell.edu>
2022-08-03 14:54:13 -0400

    commit

--- site.js
+++ site.js
@@ -1,22 +1,11 @@
-// $(document).ready(function(){
-//         var scroll_pos = 0;
-//         $(document).scroll(function() {
-//             scroll_pos = $(this).scrollTop();
-//             if(scroll_pos > 650) {
-//                 $('.hover-reveal').css('color', '#031036');
-//             } else {
-//                 $('.hover-reveal').css('color', '#fff');
-//             }
-//         });
-//     });
+var y = $(window).scrollTop();
 
-    $(document).ready(function(){
-      $(window).scroll(function(){
-      	var scroll = $(window).scrollTop();
-    	  if (scroll > 650) {
-    	    $("#menubuttons").css("background" , "blue");
-    	  }
-
-    	  else{
-    		  $("#menubuttons").css("background" , "#333");  	
-    	  }
+$(document).ready(function(){
+	$('#arrow').fadeIn();
+	$('#arrow').click(function(){
+ 		$('html, body').animate({ scrollTop: y + $(window).height() }) 
+  	});
+  	$("div > button").hover(function(){
+		$(" div > button").css("background color", "white")
+  	});
+});
